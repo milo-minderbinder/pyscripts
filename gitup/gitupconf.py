9f3279da70bc53800128889479dc06640a8f6336
milo-minderbinder <minderbinder.enterprises@gmail.com>
2015-04-28 17:32:18 -0400

    Added development projects to gitupconf

--- gitup/gitupconf.py
+++ gitup/gitupconf.py
@@ -1,7 +1,11 @@
 import os.path
 
+
+projects = []
+
+
 docker_projects_dir = os.path.expanduser('~/Docker-Projects')
-projects = [
+docker_projects = [
     {
         'name': 'baseimage',
         'url': 'git@github.com:milo-minderbinder/docker-baseimage.git',
@@ -69,3 +73,48 @@ projects = [
         'branches': ['master']
     }
 ]
+
+
+dev_projects_dir = os.path.expanduser('~/Projects')
+dev_projects = [
+    {
+        'name': 'dev-environment',
+        'url': 'git@github.com:milo-minderbinder/dev-environment.git',
+        'dir': os.path.join(dev_projects_dir, 'dev-environment'),
+        'branches': ['master']
+    },
+    {
+        'name': 'policy',
+        'url': 'git@github.com:milo-minderbinder/policy.git',
+        'dir': os.path.join(dev_projects_dir, 'policy'),
+        'branches': ['master']
+    },
+    {
+        'name': 'pyreview',
+        'url': 'git@bitbucket.org:milo-minderbinder/pyreview.git',
+        'dir': os.path.join(dev_projects_dir, 'pyreview'),
+        'branches': ['master']
+    },
+    {
+        'name': 'pyscripts',
+        'url': 'git@github.com:milo-minderbinder/pyscripts.git',
+        'dir': os.path.join(dev_projects_dir, 'pyscripts'),
+        'branches': ['master', 'indev']
+    },
+    {
+        'name': 'spring-ref',
+        'url': 'git@github.com:milo-minderbinder/spring-ref.git',
+        'dir': os.path.join(dev_projects_dir, 'spring-ref'),
+        'branches': ['master', 'indev']
+    },
+    {
+        'name': 'struts2-ref',
+        'url': 'git@github.com:milo-minderbinder/struts2-ref.git',
+        'dir': os.path.join(dev_projects_dir, 'struts2-ref'),
+        'branches': ['master']
+    }
+]
+
+
+projects.extend(docker_projects)
+projects.extend(dev_projects)
