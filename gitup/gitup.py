2a7e2d06ff16c1994cf28afba7048b365848d7b1
Milo Minderbinder <minderbinder.enterprises@gmail.com>
2015-04-29 21:58:32 -0400

    gitup info command now properly checks if a project directory exists before attempting to chdir to it, and logs a warning if the directory isn't found

--- gitup/gitup.py
+++ gitup/gitup.py
@@ -46,19 +46,23 @@ def clone_projects():
 def git_info():
     for project in projects:
         output = []
-        output.append('Git info for %s' % project['name'])
         project_dir = os.path.realpath(project['dir'])
-        os.chdir(project_dir)
-        # Fetch changes to origin and prune deleted branches
-        output.append(read_process(['git', 'fetch', '--all', '-p']))
-        # Get branch information for local and remote
-        output.append('Branch info:')
-        output.append(read_process(['git', 'branch', '-vv', '-a']))
-        # Get status for each branch
-        for branch in project['branches']:
-            output.append('Checking out branch: %s' % branch)
-            output.append(read_process(['git', 'checkout', branch]))
-            output.append('Status:')
-            output.append(read_process(['git', 'status']))
+        if os.path.isdir(project_dir):
+            output.append('Git info for %s' % project['name'])
+            os.chdir(project_dir)
+            # Fetch changes to origin and prune deleted branches
+            output.append(read_process(['git', 'fetch', '--all', '-p']))
+            # Get branch information for local and remote
+            output.append('Branch info:')
+            output.append(read_process(['git', 'branch', '-vv', '-a']))
+            # Get status for each branch
+            for branch in project['branches']:
+                output.append('Checking out branch: %s' % branch)
+                output.append(read_process(['git', 'checkout', branch]))
+                output.append('Status:')
+                output.append(read_process(['git', 'status']))
+        else:
+            log.warn('No git project named %s found in %s'
+                     % (project['name'], project_dir))
         output.append('-' * 80)
         log.info('\n'.join(output))
